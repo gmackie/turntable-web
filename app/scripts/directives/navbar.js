5635fdf09d120c5ddf9d43cb219fd9051a323dcf
Zach Zimbler <zachzimbler@gmail.com>
2014-05-08 14:41:34 -0400

    removed user voice stuff

--- app/scripts/directives/navbar.js
+++ app/scripts/directives/navbar.js
@@ -20,14 +20,6 @@ app.directive('navbar',['$rootScope', 'loginService', function ($rootScope, logi
 					scope.showLogout = false;
 				} else {
 					scope.showLogout = true;
-					try {
-						UserVoice.push(['identify', {
-							email: $rootScope.auth.user.email,
-							id: $rootScope.auth.user.uid 
-						}]);
-					} catch(e) {
-						console.log('Could not set User Voice User Data')
-					}
 				}
 			});
 		
