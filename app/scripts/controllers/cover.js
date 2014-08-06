e509f51873bfe016cb5c6f30a7f188134e88cb01
Graham Mackie <mackieg@umich.edu>
2014-08-06 12:08:30 -0400

    navbar not working

--- app/scripts/controllers/cover.js
+++ app/scripts/controllers/cover.js
@@ -2,9 +2,20 @@
 
 angular.module('angFireApp.controllers.cover', [])
 .controller('CoverCtrl', ['$scope', '$rootScope', function ($scope, $rootScope) {
-
+  console.log(Object.getOwnPropertyNames($scope));
+  console.log(Object.getOwnPropertyNames($rootScope));
+  console.log(Object.getOwnPropertyNames($rootScope.auth));
 	// Pull users data	
 	//$scope.data = $firebase(Users.find($rootScope.auth.user.uid));
 	//$scope.data.$bind($scope, 'user');
-
+  $rootScope.user = {};
+  $rootScope.user.loggedin = ($rootScope.auth && $rootScope.auth.user &&
+    $rootScope.auth.user !== "null");
+  if($rootScope.user.loggedin)
+  {
+    console.log(Object.getOwnPropertyNames($rootScope.auth.user));
+    console.log($rootScope.auth.user.email);
+    console.log($rootScope.auth.user.id);
+    console.log($rootScope.user.loggedin);
+  }
 }]);
